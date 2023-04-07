f45fbe6dba27158c490ae4e1d7cd9a713437040a
Alissonperez11 <94927353+Alissonperez11@users.noreply.github.com>
2023-04-07 09:10:15 -0500

    Add files via upload

---
+++ PRACTICA 1/Practica 1.js
@@ -0,0 +1,113 @@
+const Paciente=[
+    {
+        id:1,
+        Nombre:"Alisson",
+        Cedula:1310824621
+    },
+    {
+        id:2,
+        Nombre:"Antonio",
+        Cedula:1303742820
+    },
+    
+    {
+        id:3,
+        Nombre:"Sara",
+        Cedula:1307837638
+    },
+    
+    {
+        id:4,
+        Nombre:"Angelica",
+        Cedula:1314564324
+    },
+    
+    {
+        id:5,
+        Nombre:"Juan",
+        Cedula:1310837346
+    },
+    
+]
+const Tipo_Examen=[
+    {
+        id:1,
+        Descripcion:"Examen de orina",
+        Indicaciones:"tomar mucha agua"
+    },
+    {
+        id:2,
+        Descripcion:"Examen de embarazo",
+        Indicaciones:"Hacerse los chequeos"
+    },
+    {
+        id:3,
+        Descripcion:"Examen de sangre",
+        Indicaciones:"Hacer ejercicio"
+    },
+    {
+        id:4,
+        Descripcion:"Examen de eses",
+        Indicaciones:"Tomar Enterogermina"
+    },
+    {
+        id:5,
+        Descripcion:"Examen de tiroides",
+        Indicaciones:"Positivo a Tiroides"
+    }
+]
+const Resultado=[
+    {
+        id:1,
+        idpaciente:1,
+        idtipo_examen:1,
+        Resultado_examen:"Positivo a Infeccion urinaria",
+        Valor_pagado:"20 dolares",
+        Observaciones:"Debe tomar mucha agua y no sentarse en lugares calientes",
+        
+    },
+    {
+        id:2,
+        idpaciente:2,
+        idtipo_examen:2,
+        Resultado_examen:"Positivo a Embarazo",
+        Valor_pagado:"30 dolares",
+        Observaciones:"Debe de permanecer en reposo",
+    },
+    {
+        id:3,
+        idpaciente:3,
+        idtipo_examen:3,
+        Resultado_examen:"Resultado de sangre",
+        Valor_pagado:"25 dolares",
+        Observaciones:" Tomar Hierro",
+    },
+    {
+        id:4,
+        idpaciente:4,
+        idtipo_examen:4,
+        Resultado_examen:"Resultado de eses",
+        Valor_pagado:"20 dolares",
+        Observaciones:"Tiene gastritis",
+    },
+    {
+        id:5,
+        idpaciente:5,
+        idtipo_examen:5,
+        Resultado_examen:"Positivo a tiroides",
+        Valor_pagado:"25 dolares",
+        Observaciones:"Tomar pastillas para controlar la tiroide",
+    },
+]
+// Sentencia repetitiva del arreglo
+for(const iterator of Resultado) {
+    auxPaciente= Paciente.find(paci=> paci.id===iterator.idpaciente)
+    auxTipo= Tipo_Examen.find(tip=> tip.id===iterator.idtipo_examen)
+    console.log(`\n*** INGRESO DE Resultados ***:
+    \tNombrePaciente: ${auxPaciente.Nombre} 
+    \tidentificacion: ${auxPaciente.Cedula}
+    \tDescripcion:${auxTipo.Descripcion}
+    \tIndicaciones: ${auxTipo.Indicaciones} 
+    \tResultados: ${iterator.Resultado_examen}`);
+    
+}
