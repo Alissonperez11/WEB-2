c9b3437a1e9fc7d31fe53f3d6d7e064e7f144f16
Alissonperez11 <94927353+Alissonperez11@users.noreply.github.com>
2023-06-24 09:44:54 -0500

    commit

---
+++ 2-PARCIAL/practica1/models/index.js
@@ -0,0 +1,10 @@
+const Paciente =  require('./paciente');
+const Tipo_Examen =  require('./tipo_examen');
+const Resultado =  require('./resultado');
+
+
+module.exports ={
+    Paciente,
+    Tipo_Examen,
+    Resultado
+}
